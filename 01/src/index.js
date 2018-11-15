9150d6979fbe4e96ce0146a0c37f83fcc5c16c02
Adam Romański <adam.romanski@netguru.pl>
2018-11-15 22:31:50 +0100

    01-finish

---
+++ 01/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    // rendered element
+    <h1>Hello world</h1>,
+    // target
+    document.getElementById('root')
+);
