894db0fea82459ddf382eaa14353852d7e0dc52e
Adam Romański <adam.romanski@netguru.pl>
2018-11-18 21:26:31 +0100

    01.4-finish

---
+++ 01/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+    <div className="wrapper">
+        <h1 className="mainHeader">Hello World</h1>
+        <h2 className="secondaryHeader">Hello eduweb</h2>
+    </div>
+);
+
+export default App;
+
--- 01/src/index.js
+++ 01/src/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => (
-    <div className="wrapper">
-        <h1 className="mainHeader">Hello World</h1>
-        <h2 className="secondaryHeader">Hello eduweb</h2>
-    </div>
-);
-
-ReactDOM.render(
-    // rendered element
-    <App />,
-    // target
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
