4884522d549d218cc3ae90e793f051ec17be5a41
Adam Romański <adam.romanski@netguru.pl>
2018-11-25 22:19:31 +0100

    02.2-finish

--- 02/src/components/ListWrapper/ListItem/ListItem.js
+++ 02/src/components/ListWrapper/ListItem/ListItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './ListItem.css';
+import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = () => (
-    <li className="listItemWrapper">
-        item1
+    <li className="listItem__wrapper">
+        <img src={danAbramovImage} className="listItem__image" />
+        <div>
+            <h2 className="listItem__name">Dan Abramov</h2>
+            <p className="listItem__description">It is a good idea to think of your PC as an office. It stores files, programs, pictures. This can be compared to an actual office’s files, machines and decorations.</p>
+            <button className="listItem__button">visit twitter page</button>
+        </div>
     </li>
 );
 
