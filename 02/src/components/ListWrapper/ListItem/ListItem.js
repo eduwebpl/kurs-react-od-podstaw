f520b2b9ea1f9d5512d6e8240bb74a8b117ae4a6
Adam Romański <adam.romanski@netguru.pl>
2018-11-26 22:36:29 +0100

    02.4-finish

--- 02/src/components/ListWrapper/ListItem/ListItem.js
+++ 02/src/components/ListWrapper/ListItem/ListItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ListItem.css';
-import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = (props) => (
     <li className="listItem__wrapper">
-        <img src={props.image} className="listItem__image" />
+        <img
+            src={props.image} className="listItem__image"
+            alt={props.name}
+        />
         <div>
             <h2 className="listItem__name">
             {props.name}
@@ -12,7 +14,7 @@ const ListItem = (props) => (
             <p className="listItem__description">
             {props.description}
             </p>
-            <button className="listItem__button">visit twitter page</button>
+            <a href={props.link} className="listItem__button">visit twitter page</a>
         </div>
     </li>
 );
--- 02/src/components/ListWrapper/ListWrapper.js
+++ 02/src/components/ListWrapper/ListWrapper.js
@@ -5,26 +5,14 @@ import { twitterAccounts } from '../../data/twitterAccounts';
 
 const ListWrapper = () => (
     <ul className="listWrapper__wrapper">
-        <ListItem
-            name={twitterAccounts[0].name}
-            description={twitterAccounts[0].description}
-            image={twitterAccounts[0].image}
-        />
-        <ListItem
-            name={twitterAccounts[1].name}
-            description={twitterAccounts[1].description}
-            image={twitterAccounts[1].image}
-        />
-        <ListItem
-            name={twitterAccounts[2].name}
-            description={twitterAccounts[2].description}
-            image={twitterAccounts[2].image}
-        />
-        <ListItem
-            name={twitterAccounts[3].name}
-            description={twitterAccounts[3].description}
-            image={twitterAccounts[3].image}
-        />
+        {twitterAccounts.map((item) => (
+            <ListItem
+                name={item.name}
+                description={item.description}
+                image={item.image}
+                link={item.twitterLink}
+            />
+        ))}
     </ul>
 );
 
