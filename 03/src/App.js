cfb39b12f8ea1d5be007d5da8bf845788dfb70b8
helloroman <helloroman.vlog@gmail.com>
2018-12-23 21:46:33 +0100

    03-finish

--- 03/src/App.js
+++ 03/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css';
+import MyComponent from './components/MyComponent/MyComponent';
 
 const App = () => (
     <div>
+        <MyComponent />
         <ListWrapper />
     </div>
 );
---
+++ 03/src/components/MyComponent/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyComponent extends React.Component { 
+    render() {
+        return (
+            <h1>Hello world</h1>
+        )
+    }
+}
+
+export default MyComponent;
