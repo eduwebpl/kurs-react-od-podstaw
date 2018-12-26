ca0102dee5250563693a1e6f4abe8fc2c406ab84
helloroman <helloroman.vlog@gmail.com>
2018-12-26 21:19:42 +0100

    03.4-finish

--- 03/src/components/ListWrapper/ListWrapper.js
+++ 03/src/components/ListWrapper/ListWrapper.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
 import './ListWrapper.css';
-import { twitterAccounts } from '../../data/twitterAccounts';
 
-const ListWrapper = () => (
+const ListWrapper = (props) => (
     <ul className="listWrapper__wrapper">
-        {twitterAccounts.map(item => (
+        {props.items.map(item => (
             <ListItem key={item.name} {...item} />
         ))}
     </ul>
