d19b19d8d158a3c106e3ee4ec30766eceb82ea24
helloroman <helloroman.vlog@gmail.com>
2018-12-29 16:32:23 +0100

    03.7

---
+++ 03/src/components/Form/Form.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Form.css";
+
+const Form = ({ submitFn }) => (
+  <div className="form__wrapper">
+    <h2>Add new twitter account</h2>
+    <form className="form__form" onSubmit={submitFn}>
+      <div className="form__item">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder=" "
+          maxLength="30"
+        />
+        <label htmlFor="name">Name</label>
+        <div className="forms__item__bar" />
+      </div>
+      <div className="form__item">
+        <input type="text" name="link" id="link" placeholder=" " />
+        <label htmlFor="link">Link</label>
+        <div className="forms__item__bar" />
+      </div>
+      <div className="form__item">
+        <input type="text" name="image" id="image" placeholder=" " />
+        <label htmlFor="image">Image</label>
+        <div className="forms__item__bar" />
+      </div>
+      <div className="form__item">
+        <textarea name="description" id="description" placeholder=" " />
+        <label htmlFor="description">Description</label>
+        <div className="forms__item__bar" />
+      </div>
+      <button className="form__button" type="submit">
+        add new item
+      </button>
+    </form>
+  </div>
+);
+
+export default Form;
