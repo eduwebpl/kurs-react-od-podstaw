b8cbcbc1390ff9dc9e6eac54d38112641f9c3169
helloroman <helloroman.vlog@gmail.com>
2019-01-08 23:23:59 +0100

    04.3

---
+++ 04/src/views/Root/Root.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./index.css";
+import { BrowserRouter, Route } from 'react-router-dom';
+import TwittersView from '../TwittersView/TwittersView';
+import ArticlesView from '../ArticlesView/ArticlesView';
+import NotesView from '../NotesView/NotesView';
+
+const initialStateItems = [
+  {
+    image: "https://pbs.twimg.com/profile_images/906557353549598720/oapgW_Fp.jpg",
+    name: "Dan Abramov",
+    description: "React core member",
+    twitterLink: "https://twitter.com/dan_abramov"
+  }
+];
+
+class Root extends React.Component {
+  state = {
+    items: [...initialStateItems],
+  };
+
+  addItem = e => {
+    e.preventDefault();
+
+    const newItem = {
+      name: e.target[0].value,
+      twitterLink: e.target[1].value,
+      image: e.target[2].value,
+      description: e.target[3].value
+    };
+
+    this.setState(prevState => ({
+      items: [...prevState.items, newItem]
+    }));
+
+    e.target.reset();
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+        <>
+          <h1>hello world</h1>
+          <Route exact path="/" component={TwittersView} />
+          <Route path="/articles" component={ArticlesView} />
+          <Route path="/notes" component={NotesView} />
+        </>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Root;
