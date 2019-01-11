5fa9fd712ce155288b003f6fd0cae9b81886fe11
helloroman <helloroman.vlog@gmail.com>
2019-01-11 21:52:35 +0100

    04.4-finish

--- 04/src/components/Navigation/Navigation.js
+++ 04/src/components/Navigation/Navigation.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.scss';
 
 const Navigation = () => (
-  <ul>
-    <li><Link to="/">Twitters</Link></li>
-    <li><Link to="/articles">Articles</Link></li>
-    <li><Link to="/notes">Notes</Link></li>
-  </ul>
+  <nav>
+    <ul className={styles.wrapper}>
+      <li className={styles.navItem}>
+      <NavLink exact
+      activeClassName={styles.navItemLinkActive}
+      className={styles.navItemLink} to="/">twitters</NavLink>
+      </li>
+      <li className={styles.navItem}>
+      <NavLink
+      activeClassName={styles.navItemLinkActive}
+      className={styles.navItemLink} to="/articles">articles</NavLink>
+      </li>
+      <li className={styles.navItem}>
+      <NavLink 
+      activeClassName={styles.navItemLinkActive}
+      className={styles.navItemLink} to="/notes">notes</NavLink>
+      </li>
+    </ul>
+  </nav>
 );
 
-export default Navigation;
+export default Navigation;  
